66e7da9b56de8704e98faf28a0cf7339d7adaae0
yangjp22 <jinyang0604@gmail.com>
2022-06-22 20:00:16 -0400

    add request body update and partially update

--- request_body_app.py
+++ request_body_app.py
@@ -6,6 +6,7 @@
 """
 
 from fastapi import FastAPI, Query, Path, Body
+from fastapi.encoders import jsonable_encoder
 from typing import Union
 from pydantic import BaseModel, Field
 
@@ -125,3 +126,42 @@ async def update_img(
         'image_id': image_id,
         'weights': weights
     }
+
+
+class UpdateItem(BaseModel):
+    name: Union[str, None] = None
+    description: Union[str, None] = None
+    price: Union[float, None] = None
+    tax: float = 10.5
+    tags: list[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/item-updates/{item_id}", response_model=UpdateItem, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.put('/item-updates/{item_id}', response_model=UpdateItem)
+async def update_item(item_id: str, item: UpdateItem):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    print(items[item_id])
+    return update_item_encoded
+
+
+@app.patch('/item-updates/{item_id}', response_model=UpdateItem)
+async def update_item_partial(item_id: str,
+                              item: UpdateItem):
+    stored_data = items[item_id]
+    stored_model = UpdateItem(**stored_data)
+    update_model = item.dict(exclude_unset=True)
+    updated_model = stored_model.copy(update=update_model)
+    items[item_id] = jsonable_encoder(updated_model)
+    return updated_model
